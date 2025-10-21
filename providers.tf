44b26bee4b6e969876bf2830bb821c0373fd2713
donadikranthi <donadikranthikumar@gmail.com>
2025-10-21 20:08:56 +0530

    main

--- providers.tf
+++ providers.tf
@@ -1,21 +1,16 @@
-terraform{
-    required_providers {
-        aws = {
-        source = "hashicorp/aws"
-        version = "6.16.0"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
     }
+  }
 
-
-backend "s3" {
-  bucket = "kranthi-remote-state"
-  key    = "kranthi-remote-demo"
-  region = "us-east-1"
-  use_lockfile = true
-  encrypt = true
-}
+  backend "s3" {
+    
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,5 @@
+bucket = "kranthi-remote-state-dev"
+key = "tfvars-multi-env-demo"
+region = "us-east-01"
+use_lockfile = true
+encrypt = true
